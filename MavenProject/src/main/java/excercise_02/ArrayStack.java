9343a28b32f0b73561f9fdb21165b2ba9d14588c
Jaroslaw_Kalemba <uzytkownik@kursGIT.pl>
2023-08-28 00:07:31 +0200

    Filling code in ArrayStack.java and Main.java - complete ex.

--- MavenProject/src/main/java/excercise_02/ArrayStack.java
+++ MavenProject/src/main/java/excercise_02/ArrayStack.java
@@ -5,8 +5,8 @@ import java.util.EmptyStackException;
 public class ArrayStack implements Stack {
 
     private String[] stackData;
-    private int buffor = 10;
-    private int top = 0;
+    private int buffor = 4;
+    private int top = -1;
 
     public ArrayStack() {
         stackData = new String[buffor];
@@ -15,22 +15,34 @@ public class ArrayStack implements Stack {
     @Override
     public void push(String object) throws FullStackException {
 
-
+        if(top >= stackData.length - 1){
+            throw new FullStackException();
+        }
+        top++;
+        stackData[top] = object;
     }
 
     @Override
     public String pop() throws EmptyStackException {
-        return null;
+        if(isEmpty()){
+            throw  new EmptyStackException();
+        }
+        //usunięcie ostatniego elementu przez zmniejszenie wielkości tablicy
+        return stackData[top--];
     }
 
     @Override
     public String peek() throws EmptyStackException {
-        return null;
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        //metoda zwraca górę stosu, ale jej nie usuwa
+        return stackData[top];
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return top == -1;
     }
 
 
--- MavenProject/src/main/java/excercise_02/Main.java
+++ MavenProject/src/main/java/excercise_02/Main.java
@@ -1,4 +1,30 @@
 package excercise_02;
 
+import java.util.EmptyStackException;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayStack arrayStack = new ArrayStack();
+
+        try {
+            arrayStack.push("Dog");
+            arrayStack.push("Airplane");
+            arrayStack.push("Phone");
+            arrayStack.push("Computer");
+
+            //usuwamy i pokazujemy co usuwamy
+            System.out.println(arrayStack.pop());
+            //zerkamy na szczyt stosu
+            System.out.println(arrayStack.peek());
+            System.out.println(arrayStack.pop());
+            System.out.println(arrayStack.pop());
+            System.out.println(arrayStack.peek());
+            System.out.println(arrayStack.pop());
+
+        } catch (FullStackException | EmptyStackException e){
+            e.printStackTrace();
+        }
+    }
 }
