3f4fc7df995889ed85d90478a6616a3ad6ce33ae
Jaroslaw_Kalemba <uzytkownik@kursGIT.pl>
2023-08-27 12:29:06 +0200

    Adding Animal and ShelterWorker

---
+++ MavenProject/src/main/java/Animal.java
@@ -0,0 +1,67 @@
+public class Animal {
+
+    private int id;
+    private String name;
+    private int age;
+    private String race;
+    private ShelterWorker shelterWorker;
+
+    public Animal(int id, String name, int age, String race, ShelterWorker shelterWorker) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.race = race;
+        this.shelterWorker = shelterWorker;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public ShelterWorker getShelterWorker() {
+        return shelterWorker;
+    }
+
+    public void setShelterWorker(ShelterWorker shelterWorker) {
+        this.shelterWorker = shelterWorker;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", race='" + race + '\'' +
+                ", shelterWorker=" + shelterWorker +
+                '}';
+    }
+}
---
+++ MavenProject/src/main/java/ShelterWorker.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShelterWorker {
+
+    private int id;
+    private String name;
+    private String surname;
+    private List<Animal> animals;
+
+    public ShelterWorker(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.animals = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+
+    @Override
+    public String toString() {
+        return "ShelterWorker{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", animals=" + animals +
+                '}';
+    }
+}
